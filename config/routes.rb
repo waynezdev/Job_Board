04254edd2350885369eea2475a489485fe9325bc
waynezdev <cas021919@coderacademy.edu.au>
2019-10-05 18:07:07 +1000

    added scaffold, materializecss and added button

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "job_boards#index"
   resources :job_boards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
